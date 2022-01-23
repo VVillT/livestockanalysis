3c0b0a7ce854e4fdb4f07a75392e51eaa5833402
VVillT <whtongwilliam@gmail.com>
2022-01-23 22:23:10 +0000

    Overwrite with final version - really to HeroKu

--- StockAnalysisJan2022.py
+++ StockAnalysisJan2022.py
@@ -37,9 +37,10 @@ This app retrieves stock prices and add basic analysis from the **YahooFinance**
 # About
 expander_bar = st.expander("About")
 expander_bar.markdown("""
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn, BeautifulSoup, requests, json, time
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn, YahooFinancials, plotly, csv, datetime
 * **Data source:** yahoofinancials.
 * **Credit:** [Streamlit YouTube examples](https://www.youtube.com/watch?v=JwSS70SZdyM&feature=youtu.be&ab_channel=freeCodeCamp.org) by [Data Professor] (https://data-professor.medium.com/)
+* **Support:** Please sponsor by visiting my [buymeacoffee] (https://www.buymeacoffee.com/thedataguy) to support my app hosting if you find it useful, ideas welcome! 
 """)
 
 
@@ -54,6 +55,9 @@ col2, col3 = st.columns((2,1))
 col1.header('Input Options')
 ticket_input = "ROO.L"
 stockYFticker = col1.text_input("Enter Yahoo ticker here. For Example: ROO.L", ticket_input)
+riskfreevalue = 0 
+riskfree_input = col1.number_input("Select the % of risk free rate", value = riskfreevalue)
+#st.sidebar.write('Market risk-free rate is : ' , "{:.2%}".format(riskfree_input / 100)) 
 ## Sidebar - Currency price unit
 #currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
 
@@ -87,8 +91,7 @@ def simpleifsloop(Stock):
     for dt in comparedates: 
         closing = Stock.iloc[abs(Stock['formatted_date'] - dt ).idxmin()+1]['close']   
         if dt >= inputdtstart:           
-            st.write('If you bought the stock on ',dt.date(), ', you will buy on the price :    ',np.round(closing , decimals =2),' versus the latest date ',   Stock.iloc[-1]['formatted_date'].date(),'price :    ',np.round(Stock.iloc[-1]['close'], decimals =2)) 
-            st.write('Return on investing would be ',np.round((Stock.iloc[-1]['close']/ closing-1)*100, decimals = 2) , '%  ')
+            st.write('If you bought the stock on ',dt.date(), ', you will buy on the price :    ',np.round(closing , decimals =2),' versus the latest date ',   Stock.iloc[-1]['formatted_date'].date(),'price :    ',np.round(Stock.iloc[-1]['close'], decimals =2) , ' | Return on investing would be ',np.round((Stock.iloc[-1]['close']/ closing-1)*100, decimals = 2) , '%  ')
         #Stock['Daily Return'].hist(bins=100, title="Distribution of daily return")
 simpleifsloop(rawdata)
 
@@ -100,7 +103,7 @@ def getFTSE100():
     ftse100 = '^FTSE'
     yahoo_financials = YahooFinancials(ftse100)
     data = yahoo_financials.get_historical_price_data(start_date=str(startdate), end_date=str(enddate), time_interval='daily')
-    df = pd.DataFrame(data[ftse100]['prices']).sort_values('date',ascending = False).reset_index()
+    df = pd.DataFrame(data[ftse100]['prices']).sort_values('date',ascending = True).reset_index()
     df['indextype'] = 'ftse100'
     df['formatted_date'] = pd.to_datetime(df['formatted_date'], format='%Y-%m-%d')
     df['month'] = df['formatted_date'].dt.month
@@ -120,7 +123,7 @@ def getFTSE250():
     ftse250 = '^FTMC'
     yahoo_financials = YahooFinancials(ftse250)
     data = yahoo_financials.get_historical_price_data(start_date=str(startdate), end_date=str(enddate), time_interval='daily')
-    df = pd.DataFrame(data[ftse250]['prices']).sort_values('date',ascending = False).reset_index()
+    df = pd.DataFrame(data[ftse250]['prices']).sort_values('date',ascending = True).reset_index()
     df['indextype'] = 'ftse250'
     df['formatted_date'] = pd.to_datetime(df['formatted_date'], format='%Y-%m-%d')
     df['month'] = df['formatted_date'].dt.month
@@ -137,22 +140,38 @@ ftse100dt = ftse100.loc[(ftse100['formatted_date'] >= inputdtstart) & (ftse100['
 ftse250 = getFTSE250()
 ftse250dt = ftse250.loc[(ftse100['formatted_date'] >= inputdtstart) & (ftse250['formatted_date'] <= inputdtend)]
 data = rawdata.loc[(rawdata['formatted_date'] >= inputdtstart) & (rawdata['formatted_date'] <= inputdtend)]
+def CombineData(stockdf,indexdf,dtstart,dtend):
+    stockdf = stockdf.loc[(stockdf['formatted_date'] >= dtstart) & (stockdf['formatted_date'] <= dtend)]
+    df1 = stockdf[['formatted_date', 'Daily Return']]
+    indexdf = indexdf.loc[(indexdf['formatted_date'] >= dtstart) & (indexdf['formatted_date'] <= dtend)]
+    df2 = indexdf[['formatted_date', 'Daily Return']]
+    df = pd.merge(df1,df2,on='formatted_date', how='inner',suffixes=('_stock', '_index')).dropna()
+    return df 
+CAPMdf = CombineData(data,ftse250dt,inputdtstart,inputdtend)
 
-st.sidebar.write("On a high level, the FTSE 100 returned ", str(np.round(((ftse100dt.iloc[-1]['close']/ftse100dt.iloc[0]['close'])-1)  * 100,decimals =2)) ,'% and the FTSE 250 returned ', str(np.round(((ftse250dt.iloc[-1]['close']/ftse250dt.iloc[0]['close'])-1)  * 100,decimals =2)), '%')
-st.sidebar.write("  and this ", stockYFticker, " has returned ", str(np.round(((data.iloc[-1]['close']/data.iloc[0]['close'])-1)  *100,decimals =2)) , '% between ', inputdtstart , " & ", inputdtend )
+# Fit a polynomial between each stock and the S&P500 (Poly with order = 1 is a straight line)
+beta, alpha = np.polyfit(CAPMdf['Daily Return_index'], CAPMdf['Daily Return_stock'], 1)
+# Assume risk free rate is zero in this case
+rf = riskfree_input / 100.00  
 
+# This is the expected return of the market 
+rm = CAPMdf['Daily Return_index'].mean() * CAPMdf['Daily Return_index'].shape[0] 
+# Calculate return for security using CAPM  
+ER = rf + ( beta * (rm-rf) ) 
+
+
+st.sidebar.write("On a high level, the FTSE 100 returned ", "{:.2%}".format((ftse100dt.iloc[-1]['close']/ftse100dt.iloc[0]['close'])-1) ,' and the FTSE 250 returned ', "{:.2%}".format((ftse250dt.iloc[-1]['close']/ftse250dt.iloc[0]['close'])-1) )
+st.sidebar.write("  and this ", stockYFticker, " has returned ", "{:.2%}".format((data.iloc[-1]['close']/data.iloc[0]['close'])-1) , 'between ', inputdtstart , " & ", inputdtend )
+st.sidebar.write("***Quick Daily Return Analysis***")
+#Avg Daily Return
+st.sidebar.write('Avg Daily Return: ',"{:.2%}".format(data['Daily Return'].mean()))
+#Std Daily Return
+st.sidebar.write('Std Daily Return: ',"{:.2%}".format(data['Daily Return'].std()))
 
 # Plot Closing Price of Query Symbol
 def price_plot(data):
   #df = pd.DataFrame(data[symbol].close)
   #fig, ax = plt.subplots(figsize=(20,4))
-#  data['Date'] = data.formatted_date
-#  #plt.fill_between(data.formatted_date, data.close, color='skyblue', alpha=0.3)
-#  plt.plot(data.formatted_date, data.close, color='skyblue', alpha=0.8)
-#  plt.xticks(rotation=90)
-#  plt.title(stockYFticker, fontweight='bold')
-#  plt.xlabel('Date', fontweight='bold')
-#  plt.ylabel('Closing Price', fontweight='bold')
     fig = px.line(title =  stockYFticker+' daily close interactive trend')  
     fig.add_scatter(x = data['formatted_date'], y = data['close'], name = stockYFticker)
     return st.plotly_chart(fig, use_container_width=True)
@@ -173,8 +192,57 @@ with st.expander("Company Info"):
     st.write('List of events on calendar:')
     st.write(ticker.calendar)
 
+#CAPM Formula: ri = rf + Bi * (rm - rf)
+with st.expander("CAPM explain"):
+    st.write("Fit a polynomial between the selected stock and the FTSE100 (Poly with order = 1 is a straight line)") 
+fig= plt.figure(figsize = (12,3))
+plt.grid()
+plt.scatter(CAPMdf['Daily Return_index'] ,CAPMdf['Daily Return_stock'] )
+plt.plot(CAPMdf['Daily Return_index'], beta * CAPMdf['Daily Return_index'] + alpha, "r--")
+#testdf.plot(kind = 'scatter', x = 'Daily Return_index', y = 'Daily Return_stock')
+st.pyplot(fig)
+
+#Beta Return: 
+with st.expander("Beta explain"):
+    st.write("Beta is a measure of the volatility or systematic risk of a security or portfolio compared to the entire market (S&P500) ")
+    st.write("Beta is used in the CAPM and describes the relationship between systematic risk and expected return for assets")
+    st.write("Beta = 1.0, this indicates that its price activity is strongly correlated with the market.") 
+    st.write("Beta < 1, indicates that the security is theoretically less volatile than the market (Ex: Utility stocks). If the stock is included, this will make the portfolio less risky compared to the same portfolio without the stock.")
+    st.write("Beta > 1, indicates that the security's price is more volatile than the market. For instance, Tesla stock beta is 1.26 indicating that it's 26% more volatile than the market. ")
+if beta > 1:    
+    st.write('Beta is', beta, 'meaning it is more volatile than the market by : ',"{:.2%}".format(beta - 1))   
+elif beta < 1 :
+    st.write('Beta is', beta, 'meaning it is less volatile than the market by : ',"{:.2%}".format(1 - beta))    
+else: 
+    st.write('Beta is', beta, 'meaning it is strongly correlated with the market.')    
+#Alpha Return:
+with st.expander("Alpha explain"):
+    st.write("Alpha describes the strategy's ability to beat the market (FTSE100)")
+    st.write("Alpha indicates the “excess return” or “abnormal rate of return,” ")
+    st.write("For example, a positive 0.175 alpha for Tesla means that the portfolio’s return exceeded the benchmark index ftse250 by 17%.")
+if alpha > 0:    
+    st.write('Alpha is', alpha , 'meaning it is exceeding the benchmark index ftse250 by: ',"{:.2%}".format(alpha))   
+elif alpha < 0 :
+    st.write('Alpha is',alpha, 'meaning it is under performing the benchmark index ftse250 by : ',"{:.2%}".format(alpha))    
+else: 
+    st.write('Alpha is',"{:.2%}".format(alpha), 'meaning it is returning about the same as the benchmark index ftse250.')    
+#st.write('Alpha is: ',"{:.2%}".format(alpha))   
+#Expected Return 
+st.write('Stock CAPM Expected Return is: ',"{:.2%}".format(ER))   
+
+#Sharp Ratio
+SR = data['Daily Return'].mean()/data['Daily Return'].std()
+ASR = (252**0.5)*SR
+with st.expander("Sharp Ratio / Annualised Sharp Ratio"):
+    st.write('****Note that the higher the ratios the better the historical return against risk ')
+    st.write('and if negative then you are making a historical loss - the risk didn''t pay off!****')
+st.write('Sharp Ratio: ',"{:.2%}".format(SR))
+st.write('Annualised Sharp Ratio: ',"{:.2%}".format(ASR))
+
+
 st.header("Random 10 day's of "+ stockYFticker)
 st.dataframe(data.sample(10))
+st.write('You can download the full daily data between ', inputdtstart ,' to ', inputdtend , 'here')
 
 def filedownload(df):
     csv = data.to_csv(index=False)
@@ -184,128 +252,4 @@ def filedownload(df):
 
 st.markdown(filedownload(data), unsafe_allow_html=True)
 
-
-## Web scraping of CoinMarketCap data
-#@st.cache
-#def load_data():
-#    cmc = requests.get('https://coinmarketcap.com')
-#    soup = BeautifulSoup(cmc.content, 'html.parser')
-
-#    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
-#    coins = {}
-#    coin_data = json.loads(data.contents[0])
-#    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
-#    for i in listings:
-#      coins[str(i['id'])] = i['slug']
-
-#    coin_name = []
-#    coin_symbol = []
-#    market_cap = []
-#    percent_change_1h = []
-#    percent_change_24h = []
-#    percent_change_7d = []
-#    price = []
-#    volume_24h = []
-
-#    for i in listings:
-#      coin_name.append(i['slug'])
-#      coin_symbol.append(i['symbol'])
-#      price.append(i['quote'][currency_price_unit]['price'])
-#      percent_change_1h.append(i['quote'][currency_price_unit]['percent_change_1h'])
-#      percent_change_24h.append(i['quote'][currency_price_unit]['percent_change_24h'])
-#      percent_change_7d.append(i['quote'][currency_price_unit]['percent_change_7d'])
-#      market_cap.append(i['quote'][currency_price_unit]['market_cap'])
-#      volume_24h.append(i['quote'][currency_price_unit]['volume_24h'])
-
-#    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
-#    df['coin_name'] = coin_name
-#    df['coin_symbol'] = coin_symbol
-#    df['price'] = price
-#    df['percent_change_1h'] = percent_change_1h
-#    df['percent_change_24h'] = percent_change_24h
-#    df['percent_change_7d'] = percent_change_7d
-#    df['market_cap'] = market_cap
-#    df['volume_24h'] = volume_24h
-#    return df
-
-#df = load_data()
-
-## Sidebar - Cryptocurrency selections
-#sorted_coin = sorted( df['Symbol'] )
-#selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)#
-
-#df_selected_coin = df[ (df['Symbol'].isin(selected_coin)) ] # Filtering data#
-
-### Sidebar - Number of coins to display
-#num_coin = col1.slider('Display Top N Coins', 1, 20, 10)
-#df_coins = df_selected_coin[:num_coin]#
-
-### Sidebar - Percent change timeframe
-#percent_timeframe = col1.selectbox('Percent change time frame',
-#                                    ['7d','24h', '1h'])
-##'% 1h','void2', '% 24h', 'void3', '% 7d'
-#percent_dict = {"7d":'% 7d',"24h":'% 24h',"1h":'% 1h'}
-#selected_percent_timeframe = percent_dict[percent_timeframe]#
-
-### Sidebar - Sorting values
-#sort_values = col1.selectbox('Sort values?', ['Yes', 'No'])#
-
-#col2.subheader('Price Data of Selected Cryptocurrency')
-#col2.write('Data Dimension: ' + str(df_selected_coin.shape[0]) + ' rows and ' + str(df_selected_coin.shape[1]) + ' columns.')#
-
-#col2.dataframe(df_coins)#
-
-## Download CSV data
-## https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-#def filedownload(df,ind):
-#    csv = df.to_csv(index=ind)
-#    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-#    href = f'<a href="data:file/csv;base64,{b64}" download="crypto.csv">Download CSV File</a>'
-#    return href#
-
-#col2.markdown(filedownload(df_selected_coin,False), unsafe_allow_html=True)#
-
-##---------------------------------#
-## Preparing data for Bar plot of % Price change
-#col2.subheader('Table of % Price Change')
-#df_change = pd.concat([df_coins.Symbol, df_coins['% 1h'], df_coins['% 24h'], df_coins['% 7d']], axis=1)
-#df_change = df_change.set_index('Symbol')
-#df_change['% 1h'] = df_change['% 1h'].astype(float)
-#df_change['% 24h'] = df_change['% 24h'].astype(float)
-#df_change['% 7d'] = df_change['% 7d'].astype(float)
-#df_change['positive_percent_change_1h'] = df_change['% 1h'].astype(float) > 0
-#df_change['positive_percent_change_24h'] = df_change['% 24h'].astype(float) > 0
-#df_change['positive_percent_change_7d'] = df_change['% 7d'].astype(float) > 0
-#col2.dataframe(df_change)#
-
-## Download CSV data
-## https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-#col2.markdown(filedownload(df_change,True), unsafe_allow_html=True)#
-
-## Conditional creation of Bar plot (time frame)
-#col3.subheader('Bar plot of % Price Change')#
-
-#if percent_timeframe == '7d':
-#    if sort_values == 'Yes':
-#        df_change = df_change.sort_values(by=['% 7d'])
-#    col3.write('*7 days period*')
-#    plt.figure(figsize=(5,10))
-#    plt.subplots_adjust(top = 1, bottom = 0)
-#    df_change['% 7d'].plot(kind='barh', color=df_change['positive_percent_change_7d'].map({True: 'g', False: 'r'}))
-#    col3.pyplot(plt)
-#elif percent_timeframe == '24h':
-#    if sort_values == 'Yes':
-#        df_change = df_change.sort_values(by=['% 24h'])
-#    col3.write('*24 hour period*')
-#    plt.figure(figsize=(5,10))
-#    plt.subplots_adjust(top = 1, bottom = 0)
-#    df_change['% 24h'].plot(kind='barh', color=df_change['positive_percent_change_24h'].map({True: 'g', False: 'r'}))
-#    col3.pyplot(plt)
-#else:
-#    if sort_values == 'Yes':
-#        df_change = df_change.sort_values(by=['% 1h'])
-#    col3.write('*1 hour period*')
-#    plt.figure(figsize=(5,10))
-#    plt.subplots_adjust(top = 1, bottom = 0)
-#    df_change['% 1h'].plot(kind='barh', color=df_change['positive_percent_change_1h'].map({True: 'g', False: 'r'}))
-#    col3.pyplot(plt)
+st.write('Many thanks for trying out the tool and please feel free to contact me on LinkedIn for recommendations or spare a coin or two [here](https://www.buymeacoffee.com/thedataguy) for hosting this tool! ')
